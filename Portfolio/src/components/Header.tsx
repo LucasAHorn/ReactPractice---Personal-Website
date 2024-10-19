7f0dde491734dd095eb59a6dc16eb5379abe6ab0
Lucas Horn <lh.lucashorn@gmail.com>
2024-10-19 01:01:13 -0500

    Fixed Header beneath static items

--- Portfolio/src/components/Header.tsx
+++ Portfolio/src/components/Header.tsx
@@ -1,18 +1,22 @@
+import { useState } from "react";
 import "../styles/Header.css";
 
 function Header(props: any) {
+    const homeBtn : HTMLElement | null = document.getElementById("HomeBtn");
+    if (homeBtn) {
+        const [ele, sele] = useState(homeBtn.style.color);
+        sele("white");
+    }
+    function goToSection(a : any) {}
 
-    function goToSection(PageSectionLocations: any) {
-        console.log("clicked on a section");
-    };
 
 
     return (
         <div className="Header">
-            <h2 className="PageSections" onClick={() => goToSection(props.PageSectionLocations[0])}>Home</h2>
-            <h2 className="PageSections" onClick={() => goToSection(props.PageSectionLocations[1])}>About</h2>
-            <h2 className="PageSections" onClick={() => goToSection(props.PageSectionLocations[2])}>Portfolio</h2>
-            <h2 className="PageSections" onClick={() => goToSection(props.PageSectionLocations[3])}>Contact</h2>
+            <h2 className="PageSections" id="HomeBtn" onClick={() => goToSection(props.PageSectionLocations[0])}>Home</h2>
+            <h2 className="PageSections" id="AboutBtn" onClick={() => goToSection(props.PageSectionLocations[1])}>About</h2>
+            <h2 className="PageSections" id="PortfolioBtn" onClick={() => goToSection(props.PageSectionLocations[2])}>Portfolio</h2>
+            <h2 className="PageSections" id="ContactBtn" onClick={() => goToSection(props.PageSectionLocations[3])}>Contact</h2>
         </div>
     );
 }
